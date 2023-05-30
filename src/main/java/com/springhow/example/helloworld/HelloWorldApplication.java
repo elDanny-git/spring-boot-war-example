f3caf7217de0db0a9218c6a7d4531984fe057b17
elDanny-git <87057622+elDanny-git@users.noreply.github.com>
2023-05-30 15:06:13 +0530

    Update HelloWorldApplication.java
    Second commit changed the output message

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "5. please visit my website learning-ocean.com";
+        return "Hy My Name is Danish, Welcome! This webpage is hosted by Apache tomcat server! Deployed using Jenkins!";
     }
 
 }
